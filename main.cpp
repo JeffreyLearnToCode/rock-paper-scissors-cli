8d5d0ad8c366229a62b2e84836d33904c9e031b1
Jeffrey Abraham <jeffrey@abrahamjeffrey.com>
2025-10-25 15:54:09 -0400

    Robot can now pick hands at random

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include <array>
+#include <random>
 #include <iostream>
 
+constexpr std::array<std::string_view, 3> hands = {"Paper", "Rock", "Scissor"};
+
+std::string_view robotDecision() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<> distrib(0, hands.size()-1);
+    const int randomNumber = distrib(gen);
+
+    std::cout << randomNumber << std::endl;
+    return hands[randomNumber];
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
