322ddc791151fc7b3e4207b6f31d61ce6eeb3eaf
Jeffrey Abraham <jeffrey@abrahamjeffrey.com>
2025-10-27 20:37:31 -0400

    Worked on the logic and started using more enum was able to create an enum to determine the choice and game results.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,18 @@
 #include <string_view>
 #include <limits>
 
-constexpr std::array<std::string_view, 3> hands = {"Paper", "Rock", "Scissor"};
+constexpr std::array<std::string_view, 3> hands = {"Rock", "Paper", "Scissor"};
 
 enum GAMERESULT {
     WIN,
     LOSE,
     DRAW
+};
+
+enum CHOICE {
+    ROCK,
+    PAPER,
+    SCISSOR,
 };
 
 constexpr int OFFSET = 1;
@@ -18,18 +24,18 @@ int robotDecision() {
     std::random_device rd;
     std::mt19937 gen(rd());
 
-    std::uniform_int_distribution<> distrib(OFFSET, hands.size());
+    std::uniform_int_distribution<> distrib(0, hands.size()-1);
     const int randomNumber = distrib(gen);
 
     return randomNumber;
 }
-void printChoose(const int &offset = 0) {
+void printChoose(const int offset = 0) {
     for (int i = 0; i < hands.size(); i++) {
         std::cout << (i + offset) << ". " << hands[i] << std::endl;
     }
 }
 
-int userDecision(const int &low, const int &high) {
+int userDecision(const int low, const int high) {
     for (;;) {
 
         if (int d; std::cin >> d &&
@@ -38,16 +44,16 @@ int userDecision(const int &low, const int &high) {
         // Error Handling
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Please enter a validate choose between " << low << " and " << high << std::endl;
+        std::cout << "Please enter a validate choose between " << low << " and " << high << ": ";
     }
 }
 
-GAMERESULT gameDecision(const int &user, const int &robot) {
+GAMERESULT gameDecision(const CHOICE user, const CHOICE robot) {
     if (user == robot) return DRAW;
 
-    if ((user == 1 && robot == 2)||
-        (user == 2 && robot == 3)||
-        (user == 3 && robot == 1)) {
+    if ((user ==  PAPER && robot == ROCK)||
+        (user == ROCK && robot == SCISSOR)||
+        (user == SCISSOR && robot == PAPER)) {
         return WIN;
     }
 
@@ -58,19 +64,21 @@ int main() {
     std::cout << "Welcome to ROCK PAPER SCISSOR" << std::endl;
     printChoose(OFFSET);
     std::cout << "Pick a choose: ";
-    const int userChoice = userDecision(OFFSET, hands.size());
+    const auto userChoice = static_cast<CHOICE>(userDecision(OFFSET, hands.size()) - OFFSET);
 
-    const int robotChoice = robotDecision();
+    const auto robotChoice = static_cast<CHOICE>(robotDecision());
 
-    if (gameDecision(userChoice, robotChoice) == WIN) {
+    GAMERESULT gameResult = gameDecision(userChoice, robotChoice);
+
+    if (gameResult == WIN) {
         std::cout << "You win!" << std::endl;
-    }else if (gameDecision(userChoice, robotChoice) == DRAW) {
+    }else if (gameResult == DRAW) {
         std::cout << "You draw!" << std::endl;
     }else {
         std::cout << "You lose!" << std::endl;
     }
-    std::cout << "Robot: " << hands[robotChoice - 1] << std::endl <<
-            "User: " << hands[userChoice - 1] << std::endl;
+    std::cout << "Robot: " << hands[robotChoice] << std::endl <<
+            "User: " << hands[userChoice] << std::endl;
 
     return 0;
 }
